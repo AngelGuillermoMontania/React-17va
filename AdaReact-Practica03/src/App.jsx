42404a006d4e05534c9668f0748fa770e8fc3209
Angel Guillermo Montania <AngelGuillermoMontania@gmail.com>
2023-07-29 00:58:53 -0300

    GOT

--- AdaReact-Practica03/src/App.jsx
+++ AdaReact-Practica03/src/App.jsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
 import charactersGOT from "./data.js";
+import Header from "./components/Header.jsx";
+import Banner from "./components/Banner.jsx";
+import AddCharacter from "./components/AddCharacter.jsx";
+import ContainCard from "./components/containCard.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   const [characters, setCharacters] = useState(charactersGOT);
-  console.log(characters);
-  return <div></div>;
+
+  return (
+    <>
+      <Header />
+      <Banner />
+      <AddCharacter />
+      <ContainCard characters={characters} />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
